daa482e04487a9743309c28b13c67436377507e8
Viktor Yakushenko <yakushenkovitek@gmail.com>
2024-06-30 22:24:52 +0100

    End of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,12 @@
 import { Card, Image, Button } from "semantic-ui-react";
-import { Activity } from "../../../models/activity";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default function ActivityDetails() {
+    const {activityStore } = useStore();
+    const {selectedActivity: activity, cancelSelectedActivity, openForm} = activityStore
+    if(!activity) return <LoadingComponent/>;
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -23,7 +22,7 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
             <Card.Content extra>
                <Button.Group widths='2'>
                 <Button onClick={() => openForm(activity.id)} basic color="blue" content="Edit"/>
-                <Button onClick={cancelSelectActivity} basic color="grey" content="Cancel"/>
+                <Button onClick={cancelSelectedActivity} basic color="grey" content="Cancel"/>
                </Button.Group>
             </Card.Content>
         </Card>
