255f11342842f588354b37e432bcc709ee337fe7
Viktor Yakushenko <yakushenkovitek@gmail.com>
2024-07-04 22:39:32 +0100

    End of section 14

---
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -0,0 +1,41 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Infrastructure.Security
+{
+    public class IsHostRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
+    {
+        private readonly DataContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public IsHostRequirementHandler(DataContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _dbContext = dbContext;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if(userId == null) return;
+
+            var activityId = Guid.Parse(_httpContextAccessor.HttpContext?
+                .Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value?.ToString());
+
+            var attendee = await _dbContext.ActivityAttendees
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activityId);
+
+            if(attendee == null) return;
+
+            if(attendee.IsHost) context.Succeed(requirement);
+        }
+    }
+}
