c2e217d3a705894cb5cc01b48e240a430e0a93a4
Viktor Yakushenko <yakushenkovitek@gmail.com>
2024-07-04 18:53:01 +0100

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../../models/user";
 
 const sleep = (delay: number) => {
     return new Promise(resolve => {
@@ -12,6 +13,16 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if(token && config.headers){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
 axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
@@ -20,7 +31,7 @@ axios.interceptors.response.use(async response => {
 
     switch (status) {
         case 400:
-            if (config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {               
+            if (config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {
                 router.navigate('/not-found');
             }
             if (data.errors) {
@@ -52,8 +63,6 @@ axios.interceptors.response.use(async response => {
     return Promise.reject(error);
 });
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
-
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
     post: <T, Y>(url: string, body: Y) => axios.post<T>(url, body).then(responseBody),
@@ -69,8 +78,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 }
 
+const Account = {
+    current: () => requests.get<User>("/account"),
+    login: (user: UserFormValues) => requests.post<User, UserFormValues>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User, UserFormValues>('/account/register', user),
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
