367db8326c5c79f40b56dac5a26161f352d1af6a
Viktor Yakushenko <yakushenkovitek@gmail.com>
2024-07-05 15:19:51 +0100

    End of section 15

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-import { Activity } from "../../models/activity";
+import { Activity, ActivityFormValues } from "../../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
@@ -17,7 +17,7 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
-    if(token && config.headers){
+    if (token && config.headers) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
@@ -73,9 +73,10 @@ const requests = {
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`),
+    create: (activity: ActivityFormValues) => requests.post<void, ActivityFormValues>('/activities', activity),
+    update: (activity: ActivityFormValues) => requests.put<void, ActivityFormValues>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void, object>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
