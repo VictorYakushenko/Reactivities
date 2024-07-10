0ebce99c5c16a0e859a7ead83b728cd9238f42c5
Viktor Yakushenko <yakushenkovitek@gmail.com>
2024-07-10 23:31:12 +0100

    End of section 19

---
+++ Application/Comments/List.cs
@@ -0,0 +1,40 @@
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentsDto>>>
+        {
+            public Guid ActivityId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<CommentsDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<List<CommentsDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.Comments
+                    .Where(x => x.Activity.Id == request.ActivityId)
+                    .OrderByDescending(x => x.CreateAt)
+                    .ProjectTo<CommentsDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentsDto>>.Success(comments);
+            }
+        }
+    }
+}
